f0ce2f1313f466c140f9b1dd2b0cbaecde1d3f61
Kahon7586 <kahon7586@gmail.com>
2023-12-25 16:12:07 +0800

    add postboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Postboard from './Components/Postboard/Postboard';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      
+      <Postboard/>
     </div>
   );
 }

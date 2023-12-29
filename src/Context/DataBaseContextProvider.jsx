e52f5a214095cf0dca9e06a2169994aad7dc64cb
Kahon7586 <kahon7586@gmail.com>
2023-12-29 16:42:58 +0800

    add readArticle function

--- src/Context/DataBaseContextProvider.jsx
+++ src/Context/DataBaseContextProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext } from 'react'
 import { db } from './FirebaseAuth';
-import { ref, set } from "firebase/database";
+import { ref, set, get } from "firebase/database";
+import Card from '../Components/Card/Card';
 
 export const DataBaseContext = createContext({});
 
@@ -17,8 +18,32 @@ export const DataBaseContextProvider = ({children}) => {
     });
   }
 
+  async function readArticle( setState ) {
+
+    get(ref(db, 'articles')).then((snapshot) => {
+      if(snapshot.exists()){
+        const result = snapshot.val()
+        console.log(result)
+
+        let temp = []
+        for (const [ID, {category, title, content}] of Object.entries(result)) {
+          const component = <Card info={ {category, title, content} } key={ID}/>
+          temp.push(component)
+        }
+
+        setState(temp)
+      }else{
+        console.log("No data")
+        setState(null)
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   const contextValue = {
-    writeArticle
+    writeArticle,
+    readArticle
   }
 
   return (

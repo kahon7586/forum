fb6808f656ee08574e3b128f1c16d4987198a474
Kahon7586 <kahon7586@gmail.com>
2024-01-01 22:39:45 +0800

    add footer for postTime

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Card = ({ info: { category, title, content} }) => {
+const Card = ({ info: { category, title, content, postTime} }) => {
+
+  const date = new Date(postTime.seconds * 1000)
+  const userLocales = navigator.language
+  
+  const formatedPostTime = date.toLocaleString(userLocales)
+
   return (
     <div className="card mb-2" >
       <p className="card-header fs-6">{category}</p>
@@ -8,6 +14,7 @@ const Card = ({ info: { category, title, content} }) => {
         <p className="card-title fs-4">{title}</p>
         <p className="card-content">{content}</p>
       </div>
+      <div className="card-footer d-flex justify-content-end text-body-secondary">{formatedPostTime}</div>
     </div>
   )
 }

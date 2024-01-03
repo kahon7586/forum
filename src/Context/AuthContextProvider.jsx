17d3469de4b794dbf2ff5f9f6c55fa0a0d1afe9d
Kahon7586 <kahon7586@gmail.com>
2024-01-03 21:27:37 +0800

    build basic setup in authContext

---
+++ src/Context/AuthContextProvider.jsx
@@ -0,0 +1,54 @@
+import { onAuthStateChanged } from 'firebase/auth'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { auth } from './FirebaseAuth'
+
+
+const AuthContext = createContext(null)
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+const AuthContextProvider = ({children}) => {
+
+  const [currentUser, setCurrentUser] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+
+    //Calling onAuthStateChanged() 
+    //1. adds an observer/listener for changes to the user's sign-in state , AND
+    //2. returns an unsubscribe function.
+
+    //Unsubscribe funtion will remove the user state listener when being called
+
+    //So this is how everything work:
+
+    //1. <AuthContextProvider> is builded when app initialized
+    //2. This useEffect is triggered. 
+    //3. call onAuthStateChanged(), which add a listener to user state, and return a unsubcribe function into const varible unsubscribe.
+    //4. When user state changed(ex: Sign in), listener triggered and set urrentUser with user. Noticed that this listener will keep existing because unsubcribe function is not being called.
+    //5. In some case(not here), when app leave from AuthContextProvider, unsubcribe will be called as clean up function, and finally remove this listener.
+
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+      setLoading(false)
+    })
+
+    return unsubscribe
+  }, [])
+
+
+  const contextValue = {
+    currentUser,
+  }
+
+  return (
+    <AuthContext.Provider value={contextValue} >
+      {loading ? null : children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContextProvider
--- src/Context/GlobalContextProvider.jsx
+++ src/Context/GlobalContextProvider.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import DataBaseContextProvider from './DataBaseContextProvider'
+import AuthContextProvider from './AuthContextProvider'
 
 const GlobalContextProvider = ( {children} ) => {
   return (
+    <AuthContextProvider>
     <DataBaseContextProvider>
       {children}
     </DataBaseContextProvider>
+    </AuthContextProvider>
   )
 }
 
